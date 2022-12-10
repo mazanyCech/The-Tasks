4b059625f38048a0e089d3bdee432312dc00ef82
mazanyCech <cyberpunk1408@gmail.com>
2022-12-10 21:03:33 +0300

    a

--- Lesson 12/task1.js
+++ Lesson 12/task1.js
@@ -1,3 +1,20 @@
+// to develop a web platform,
+// that will track the students' progress in learning web development.
+// First you need to create a student object with 3 properties:
+// 1. stack. An array of strings, where each string is a technology. Initially the array
+// array must be equal to ['HTML'].
+// 2. level. The data type is a number. The level is responsible for the current level of the student and is initially
+// and is initially equal to 1.
+// 3. improveLevel. The function which first increases the level property of a student by 1.
+// of the student's level property by one. If the level is 2, you need to add the value of 'CSS' to the end
+// CSS' at the end of the stack; if 3rd, you need to add 'JavaScript' value to the end of the stack.
+// JavaScript', 4th is 'React', 5th is 'NodeJS'. If the value of level became more than 5, then
+// you should print the message "The student has learned
+// all technologies!". Also, the improveLevel function should at the very end return
+// the updated student object.
+// As a result, the value of the stack property after executing the code below should be as follows.
+// ["HTML", "CSS", "JavaScript", "React", "NodeJS"].
+
 const student = {
   stack: ["HTML"],
   level: 1,
